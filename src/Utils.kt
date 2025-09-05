57e23b3b6528c4a2c488c6d933b7183007cf3f1c
Miguel Del Corso <migueldelcorso@gmail.com>
2025-09-05 11:28:29 -0300

    Added utils and created the nextDay task

--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,73 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+/**
+ * Represents the directions on a grid
+ */
+enum class Directions(val rowDelta: Int, val colDelta: Int) {
+    UP(-1, 0),
+    DOWN(1, 0),
+    LEFT(0, -1),
+    RIGHT(0, 1)
+}
+
+fun <T> List<T>.combinations(k: Int): List<List<T>> {
+    val results = mutableListOf<List<T>>()
+
+    fun backtrack(start: Int, current: MutableList<T>) {
+        if (current.size == k) {
+            results.add(current.toList())
+            return
+        }
+        for (i in start..this.lastIndex) {
+            current.add(this[i])
+            backtrack(i + 1, current)
+            current.removeLast()
+        }
+    }
+    backtrack(0, mutableListOf())
+    return results
+}
+
+fun <T> List<T>.permutations(): List<List<T>> {
+    if (size <= 1) return listOf(this)
+
+    val result = mutableListOf<List<T>>()
+    for (i in indices) {
+        val element = this[i]
+        val remaining = this.toMutableList().apply { removeAt(i) }
+        val perms = remaining.permutations()
+        result.addAll(perms.map { listOf(element) + it })
+    }
+    return result
+}
+
+fun <T> List<T>.prettyPrint() {
+    this.forEach { println(it) }
+    println()
+}
+
+fun Char.offsetLetter(offset: Int): Char {
+    val lower = this.lowercaseChar()
+    if(lower == '-') return ' '
+
+    val baseP = lower - 'a'
+    val newP = (baseP + offset) % 26
+
+    val wrappedPosition = if (newP < 0) newP + 26 else newP
+    return 'a' + wrappedPosition
+}
+
+fun <T> List<List<T>>.transpose(): MutableList<MutableList<T>> {
+    if (this.isEmpty()) return mutableListOf()
+
+    val rows = this.size
+    val cols = this[0].size
+
+    return MutableList(cols) { col ->
+        MutableList(rows) { row ->
+            this[row][col]
+        }
+    }
+}
