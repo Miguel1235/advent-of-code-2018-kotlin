41b5f1333d361153e8f3f12ef10b57adff99a11d
Miguel Del Corso <migueldelcorso@gmail.com>
2025-09-06 18:08:05 -0300

    Finished first day

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,27 @@
+fun part1(input: List<String>): Int {
+    return input.sumOf { it.toInt() }
+}
+
+fun part2(input: List<String>): Int {
+    val input = input.map { it.toInt() }
+    val frequencies = mutableListOf(0)
+    var index = 0
+
+    while (true) {
+        val change = input[index % input.size]
+        val resultingFrequency = frequencies.last() + change
+        if (frequencies.contains(resultingFrequency)) return resultingFrequency
+        frequencies.add(resultingFrequency)
+        index++
+    }
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    check(part1(listOf("test_input")) == 1)
-
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
+    check(part2(testInput) == 2)
+
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    check(part1(input) == 599)
+    check(part2(input) == 81204)
 }
