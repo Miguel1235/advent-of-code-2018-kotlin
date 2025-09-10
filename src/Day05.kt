46f26574630aae53736d98dc9edd7208822cadbe
Miguel Del Corso <migueldelcorso@gmail.com>
2025-09-10 14:16:25 -0300

    Removed unnecessary imports and formatted again

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,6 +1,3 @@
-import kotlin.text.first
-import kotlin.text.last
-
 fun hasOppositePolarity(f: Char, s: Char) = (f.isLowerCase() && s.isUpperCase()) || (f.isUpperCase() && s.isLowerCase())
 private fun compressString(polymer: String): String {
     if (polymer.isEmpty()) return ""
@@ -19,15 +16,17 @@ private fun compressString(polymer: String): String {
 }
 
 private val part1 = { input: String -> compressString(input).count() }
-private val part2 = { input: String -> input.lowercase().toSet().sorted().minOf {
-    compressString(input.replace(Regex("[${it.lowercaseChar()}${it.uppercaseChar()}]"), "")).count()
-}}
+private val part2 = { input: String ->
+    input.lowercase().toSet().sorted().minOf {
+        compressString(input.replace(Regex("[${it.lowercaseChar()}${it.uppercaseChar()}]"), "")).count()
+    }
+}
 
 fun main() {
     val testInput = readInput("Day05_test").first()
     check(part1(testInput) == 10)
     check(part2(testInput) == 4)
-     
+
     val input = readInput("Day05").first()
     check(part1(input) == 10804)
     check(part2(input) == 6650)
