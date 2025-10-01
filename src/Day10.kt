25b1e38d65f2f7c2123c160c68077743f57ced16
Miguel Del Corso <migueldelcorso@gmail.com>
2025-10-01 16:01:27 -0300

    Finished Day10.kt

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,9 +1,3 @@
-
-import java.awt.Color
-import java.awt.image.BufferedImage
-import java.io.File
-import javax.imageio.ImageIO
-
 data class Point(var r: Int, var c: Int, var rs: Int, var cs: Int) {
     fun move() {
         r += rs
@@ -11,46 +5,36 @@ data class Point(var r: Int, var c: Int, var rs: Int, var cs: Int) {
     }
 }
 
-
-
-fun createImageFromPoints(points: List<Point>, outputPath: String) {
-    if (points.isEmpty()) return
-
+fun visualizePoints(points: List<Point>, iteration: Int) {
     val minR = points.minOf { it.r }
     val maxR = points.maxOf { it.r }
     val minC = points.minOf { it.c }
     val maxC = points.maxOf { it.c }
 
-    val width = (maxC - minC + 1)
-    val height = (maxR - minR + 1)
+    val height = maxR - minR + 1
+    val width = maxC - minC + 1
 
-    val occupied = points.map { it.r to it.c }.toSet()
-
-    val image = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
-    val graphics = image.createGraphics()
-
-    graphics.color = Color.WHITE
-    graphics.fillRect(0, 0, image.width, image.height)
-    graphics.color = Color.BLACK
-    for (r in minR..maxR) {
-        for (c in minC..maxC) {
-            if (occupied.contains(r to c)) {
-                val x = (c - minC)
-                val y = (r - minR)
-                graphics.fillRect(x, y,1,1)
-            }
-        }
+    if (height > 20 || width > 80) {
+        return
     }
 
-    graphics.dispose()
+    val grid = Array(height) { CharArray(width) { ' ' } }
 
-    ImageIO.write(image, "PNG", File(outputPath))
-    println("Image saved to $outputPath (${image.width}x${image.height})")
+    for (point in points) {
+        val row = point.r - minR
+        val col = point.c - minC
+        grid[row][col] = '#'
+    }
+
+    println("it: $iteration")
+    for (row in grid) {
+        println(row.joinToString(""))
+    }
 }
 
 private fun part1(points: List<Point>): Int {
-    repeat(4) { iteration ->
-        createImageFromPoints(points, "output$iteration.png")
+    repeat(1_000_000) {
+        visualizePoints(points, it)
         points.forEach { it.move() }
     }
     return 0
@@ -65,8 +49,8 @@ private val parseInput = { input: List<String> ->
 }
 
 fun main() {
-//    val testInput = parseInput(readInput("Day10_test"))
-//    check(part1(testInput) == 0)
+    val testInput = parseInput(readInput("Day10_test"))
+    check(part1(testInput) == 0)
 
     val input = parseInput(readInput("Day10"))
     part1(input)
