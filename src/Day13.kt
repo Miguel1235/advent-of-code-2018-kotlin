0bf0216846f056217ab5b059628cffc88cf47f60
Miguel Del Corso <migueldelcorso@gmail.com>
2025-10-29 18:16:24 -0300

    Finished first pary of the day13

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,16 +1,17 @@
 
-private fun part1(input: List<String>): Int {
+private fun part1(input: List<String>): Pair<Int, Int> {
     val grid = input.map { it.toList() }
     val cartSigns = mutableListOf<Char>('<', '>', '^', 'v')
 
-    data class Path(val r: Int, val c: Int, val pic: Char) {
-        override fun toString(): String {
-            return pic.toString()
-        }
-    }
-
+    data class Path(val r: Int, val c: Int, val pic: Char)
 
     data class Cart(var r: Int, var c: Int, var direction: Directions) {
+        val intersectionSigns = listOf("left", "straight", "right")
+        val infiniteSignSequence = generateSequence(0) { (it + 1) % intersectionSigns.size }
+            .map { intersectionSigns[it] }
+        val iterator = infiniteSignSequence.iterator()
+
+
         fun moveCart(track: List<Path>) {
             val currentTrack = track.first { it.r == r && it.c == c }
             when (currentTrack.pic) {
@@ -27,6 +28,24 @@ private fun part1(input: List<String>): Int {
                     Directions.DOWN -> Directions.RIGHT
                     Directions.RIGHT -> Directions.DOWN
                 }
+
+                '+' -> {
+                    when(iterator.next()) {
+                        "left" -> direction = when (direction) {
+                            Directions.UP -> Directions.LEFT
+                            Directions.RIGHT -> Directions.UP
+                            Directions.DOWN -> Directions.RIGHT
+                            Directions.LEFT -> Directions.DOWN
+                        }
+                        "straight" -> direction = direction
+                        "right" -> direction = when (direction) {
+                            Directions.UP -> Directions.RIGHT
+                            Directions.RIGHT -> Directions.DOWN
+                            Directions.DOWN -> Directions.LEFT
+                            Directions.LEFT -> Directions.UP
+                        }
+                    }
+                }
             }
             when (direction) {
                 Directions.UP -> r--
@@ -35,6 +54,24 @@ private fun part1(input: List<String>): Int {
                 Directions.RIGHT -> c++
             }
         }
+    }
+
+
+    fun printGrid(paths: List<Path>, carts: List<Cart> = listOf()) {
+        val maxRow = paths.maxOf { it.r }
+        val maxCol = paths.maxOf { it.c }
+        val grid = Array(maxRow + 1) { CharArray(maxCol + 1) { ' ' } }
+
+        for (p in paths) grid[p.r][p.c] = p.pic
+
+        for(cart in carts) grid[cart.r][cart.c] = when(cart.direction) {
+            Directions.UP -> '^'
+            Directions.DOWN -> 'v'
+            Directions.LEFT -> '<'
+            Directions.RIGHT -> '>'
+        }
+
+        for (row in grid) println(row.concatToString())
     }
 
 
@@ -59,19 +96,29 @@ private fun part1(input: List<String>): Int {
         for(r in grid.indices) {
             for(c in grid[r].indices) {
                 add(when (val item = grid[r][c]) {
-                    'v', '^' -> Path(r,c, grid[r-1][c])
-                    '<', '>' -> Path(r,c, grid[r][c-1])
+                    'v', '^' -> Path(r,c, '|')
+                    '<', '>' -> Path(r,c, '-')
+                    '+' -> Path(r,c, '+')
                     else -> Path(r,c, item)
                 })
             }
         }
     }.filter { it.pic != ' ' }
 
-    println(tracks)
-    tracks.prettyPrint()
-    println(carts)
 
-    return 0
+
+
+    while(true) {
+        carts.forEach { it.moveCart(tracks) }
+        val positionsGrouped = carts.groupingBy { it.r to it.c }.eachCount()
+
+        val r = positionsGrouped.filter { it.value > 1 }
+        if(r.isNotEmpty()) {
+            val (r,c) = r.keys.first()
+            return Pair(c,r)
+        }
+    }
+
 }
 
 private fun part2(input: List<String>): Int {
@@ -79,12 +126,12 @@ private fun part2(input: List<String>): Int {
 }
 
 fun main() {
-    val testInput = readInput("Day13_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+    val testInput = readInputNoTrim("Day13_test")
+    check(part1(testInput) == Pair(7,3))
+//    check(part2(testInput) == 0)
      
-//    val input = readInput("Day13")
-//    check(part1(input) == 0)
+    val input = readInputNoTrim("Day13")
+    check(part1(input) == Pair(91, 69))
 //    check(part2(input) == 0)
 }
  
